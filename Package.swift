036fc2785ebca81c5423aa51eb0bc92a1f56f2fb
Chris Marshall <chris@riftvalleysoftware.com>
2021-07-13 06:28:07 -0400

    Moved the plist, so it won't show in the test report.

--- Package.swift
+++ Package.swift
@@ -1,26 +1,22 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.4
 
 import PackageDescription
 
 let package = Package(
     name: "RVS_GeneralObserver",
     platforms: [
-        .iOS(.v13),
-        .tvOS(.v13),
-        .macOS(.v10_15),
+        .iOS(.v14),
+        .tvOS(.v14),
+        .macOS(.v11),
         .watchOS(.v6)
     ],
     products: [
         .library(name: "RVS-GeneralObserver",
-                 targets: [
-                    "RVS_GeneralObserver"
-                    ]
+                 targets: ["RVS_GeneralObserver"]
         )
     ],
     targets: [
-        .target(
-            name: "RVS_GeneralObserver",
-            dependencies: []),
+        .target(name: "RVS_GeneralObserver"),
         .testTarget(
             name: "RVS_GeneralObserverTest",
             dependencies: ["RVS_GeneralObserver"])
